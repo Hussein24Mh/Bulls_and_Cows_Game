907892221875ac0f8baf6bb7440911f217b2e7d8
Hussein24Mh <hussein24mh@gmail.com>
2022-02-04 22:15:08 +0200

    semi final version

--- pycharmcode.py
+++ pycharmcode.py
@@ -1,25 +1,26 @@
 from random import choice
-# //////////////////////////////////////////////////////////////////
-Ap1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-Ap2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-Ap3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-Ap4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-Bulls_1 = []
-Bulls_2 = []
-Bulls_3 = []
+Ap1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # All current choices in position 1
+Ap2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # All current choices in position 2
+Ap3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # All current choices in position 3
+Ap4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # All current choices in position 4
 
-All_Guesses = []
-Current_Guess = []
-p1, p2, p3, p4 = 0, 0, 0, 0
-S = ''
-loop_flag = 1
-# //////////////////////////////////////////////////////////////////
-def Generate_New_Guess():
+Bulls_1 = []  # All guesses with 1 Bull
+Bulls_2 = []  # All guesses with 2 Bull
+Bulls_3 = []  # All guesses with 3 Bull
+
+All_Guesses = []    # All guesses over time to prevent generate the same Guess
+Current_Guess = []  # current generated guess
+p1, p2, p3, p4 = 0, 0, 0, 0  # each position guess
+S = ''                       # the guess as string variable to print it
+loop_flag = 1                # loop counter
+
+
+def generate_new_guess():
     """
     generate new guesses and over time these guesses choices will be
-    reduced since we take and implement information from the user
-    :rtype: list
+    reduced since we take information from the user and make implementation
+    on them
     """
     global Current_Guess, S, p1, p2, p3, p4
 
@@ -31,104 +32,104 @@ def Generate_New_Guess():
     Current_Guess = [p1, p2, p3, p4]
 
     if Current_Guess in All_Guesses:
-        Generate_New_Guess()
+        generate_new_guess()
     else:
         All_Guesses.append(Current_Guess)
         S = str(p1) + str(p2) + str(p3) + str(p4)
-# //////////////////////////////////////////////////////////////////
-def Try_Reduce_Choices():
+
+
+def try_reduce_choices():
     """
-    # find_patterns function try to reduce every position choices
-    # based on information about bulls and cows it gets from the user
-    :return: Nothing
+    find_patterns function try to reduce every position choices
+    based on information about bulls and cows it gets from the user
     """
     global Ap1, Ap2, Ap3, Ap4
 
-    for i1 in range(0, len(Bulls_1)):
+    for i1 in range(0, len(Bulls_1)):  # Bulls_1
         fp1, fp2, fp3, fp4 = Bulls_1[i1]
-# ------------------------------------------ 1
+
         if all([fp1 in Ap1,
                 fp2 not in Ap2,
                 fp3 not in Ap3,
                 fp4 not in Ap4
                 ]):
-            Ap1 = [fp1]
-# ------------------------------------------ 2
+            Ap1 = [fp1]  # 1
+
         elif all([fp2 in Ap2,
                   fp1 not in Ap1,
                   fp3 not in Ap3,
                   fp4 not in Ap4
                   ]):
-            Ap2 = [fp2]
-# ------------------------------------------ 3
+            Ap2 = [fp2]  # 2
+
         elif all([fp3 in Ap3,
                   fp1 not in Ap1,
                   fp2 not in Ap2,
                   fp4 not in Ap4
                   ]):
-           Ap3 = [fp3]
-# ------------------------------------------ 4
+            Ap3 = [fp3]  # 3
+
         elif all([fp4 in Ap4,
                   fp1 not in Ap1,
                   fp2 not in Ap2,
                   fp3 not in Ap3
                   ]):
-            Ap4 = [fp4]
-# ---------------------------------------------------------------
-    for i2 in range(0, len(Bulls_2)):
+            Ap4 = [fp4]  # 4
+
+    for i2 in range(0, len(Bulls_2)):  # Bulls_2
         fp1, fp2, fp3, fp4 = Bulls_2[i2]
-# ------------------------------------------ 1&2
+
         if all([fp1 in Ap1,
                 fp2 in Ap2,
                 fp3 not in Ap3,
                 fp4 not in Ap4
                 ]):
             Ap1 = [fp1]
-            Ap2 = [fp2]
-# ------------------------------------------ 1&3
+            Ap2 = [fp2]  # 1&2
+
         elif all([fp1 in Ap1,
                   fp3 in Ap3,
                   fp2 not in Ap2,
                   fp4 not in Ap4
                   ]):
             Ap1 = [fp1]
-            Ap3 = [fp3]
-# ------------------------------------------ 1&4
+            Ap3 = [fp3]  # 1&3
+
         elif all([fp1 in Ap1,
                   fp4 in Ap4,
                   fp2 not in Ap2,
                   fp3 not in Ap3
                   ]):
             Ap1 = [fp1]
-            Ap4 = [fp4]
-# ------------------------------------------ 2&3
+            Ap4 = [fp4]  # 1&4
+
         elif all([fp2 in Ap2,
                   fp3 in Ap3,
                   fp1 not in Ap1,
                   fp4 not in Ap4
                   ]):
             Ap2 = [fp2]
-            Ap3 = [fp3]
-# ------------------------------------------ 2&4
+            Ap3 = [fp3]  # 2&3
+
         elif all([fp2 in Ap2,
                   fp4 in Ap4,
                   fp1 not in Ap1,
                   fp3 not in Ap3
                   ]):
             Ap2 = [fp2]
-            Ap4 = [fp4]
-# ------------------------------------------ 3&4
+            Ap4 = [fp4]  # 2&4
+
         elif all([fp3 in Ap3,
                   fp4 in Ap4,
                   fp1 not in Ap1,
                   fp2 not in Ap2
                   ]):
             Ap3 = [fp3]
-            Ap4 = [fp4]
-# ---------------------------------------------------------------
-    for i3 in range(0, len(Bulls_3)):
+            Ap4 = [fp4]  # 3&4
+
+    for i3 in range(0, len(Bulls_3)):  # Bulls_3
         fp1, fp2, fp3, fp4 = Bulls_3[i3]
-# ------------------------------------------ 2&3&4
+
         if all([fp1 not in Ap1,
                 fp2 in Ap2,
                 fp3 in Ap3,
@@ -136,8 +137,8 @@ def Try_Reduce_Choices():
                 ]):
             Ap2 = [fp2]
             Ap3 = [fp3]
-            Ap4 = [fp4]
-# ------------------------------------------ 1&3&4
+            Ap4 = [fp4]  # 2&3&4
+
         elif all([fp2 not in Ap2,
                   fp1 in Ap1,
                   fp3 in Ap3,
@@ -145,8 +146,8 @@ def Try_Reduce_Choices():
                   ]):
             Ap1 = [fp1]
             Ap3 = [fp3]
-            Ap4 = [fp4]
-# ------------------------------------------ 1&2&4
+            Ap4 = [fp4]  # 1&3&4
+
         elif all([fp3 not in Ap3,
                   fp1 in Ap1,
                   fp2 in Ap2,
@@ -154,8 +155,8 @@ def Try_Reduce_Choices():
                   ]):
             Ap1 = [fp1]
             Ap2 = [fp2]
-            Ap4 = [fp4]
-# ------------------------------------------ 1&2&3
+            Ap4 = [fp4]  # 1&2&4
+
         elif all([fp4 not in Ap4,
                   fp1 in Ap1,
                   fp2 in Ap2,
@@ -163,13 +164,14 @@ def Try_Reduce_Choices():
                   ]):
             Ap1 = [fp1]
             Ap2 = [fp2]
-            Ap3 = [fp3]
-# //////////////////////////////////////////////////////////////////
+            Ap3 = [fp3]  # 1&2&3
+
+
 while True:
-    # -----------------------------------------------------------------
-    Generate_New_Guess()
+    generate_new_guess()
     bulls, cows = 0, 0
     print(f'Guess -> {loop_flag} is {S}')
+
     print(f'Enter Bulls Number ->>>', end=" ")
     while True:
         try:
@@ -177,11 +179,12 @@ while True:
             if not (0 <= x <= 4):
                 print("Please enter integer between 0 and 4")
                 continue
-        except:
+        except TypeError:
             print("please enter integer")
         else:
             bulls = x
             break
+
     print(f'Enter Cows Number ->>>', end=" ")
     while True:
         try:
@@ -192,15 +195,16 @@ while True:
                     "and cows together between 0 and 4"
                       )
                 continue
-        except:
+        except TypeError:
             print("please enter integer")
         else:
             cows = x
             break
+
     if bulls == 4:
         print("End Of Game :GG")
         break
-    # -----------------------------------------------------------------
+# -----------------------------------------------------------------
     if bulls == 0:
         Ap1.remove(p1)
         Ap2.remove(p2)
@@ -218,15 +222,14 @@ while True:
         Ap2 = [n for n in Ap2 if n in Current_Guess]
         Ap3 = [n for n in Ap3 if n in Current_Guess]
         Ap4 = [n for n in Ap4 if n in Current_Guess]
-    # -----------------------------------------------------------------
+# -----------------------------------------------------------------
     if bulls == 1:
         Bulls_1.append(Current_Guess)
     elif bulls == 2:
         Bulls_2.append(Current_Guess)
     elif bulls == 3:
         Bulls_3.append(Current_Guess)
-    # -----------------------------------------------------------------
-    Try_Reduce_Choices()
+# -----------------------------------------------------------------
+    try_reduce_choices()
     loop_flag += 1
 # End of while loop
-# ///////////////////////////////////////////////////////////////////////////
