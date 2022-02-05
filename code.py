b8adcf6e11ca4b37a649503f69f9fd305ce0dcb8
Hussein24Mh <hussein24mh@gmail.com>
2022-02-05 11:27:24 +0200

    add some function to make the algorithm smarter and faster

--- code.py
+++ code.py
@@ -9,11 +9,54 @@ Bulls_1 = []  # All guesses with 1 Bull
 Bulls_2 = []  # All guesses with 2 Bull
 Bulls_3 = []  # All guesses with 3 Bull
 
-All_Guesses = []    # All guesses over time to prevent generate the same Guess
+Bulls_1_Cows_0 = []  # All guesses with 1 Bull and 0 Cows
+Bulls_2_Cows_0 = []  # All guesses with 2 Bull and 0 Cows
+Bulls_3_Cows_0 = []  # All guesses with 3 Bull and 0 Cows
+
+All_Guesses = []  # All guesses over time to prevent generate the same Guess
 Current_Guess = []  # current generated guess
 p1, p2, p3, p4 = 0, 0, 0, 0  # each position guess
-S = ''                       # the guess as string variable to print it
-loop_flag = 1                # loop counter
+S = ''  # the guess as string variable to print it
+loop_flag = 1  # loop counter
+
+Len_Ap1 = 10  # len(Ap1)
+Len_Ap2 = 10  # len(Ap2)
+Len_Ap3 = 10  # len(Ap3)
+Len_Ap4 = 10  # len(Ap4)
+
+Final_Ap1 = False  # True only if len(Ap1) == 1
+Final_Ap2 = False  # True only if len(Ap2) == 1
+Final_Ap3 = False  # True only if len(Ap3) == 1
+Final_Ap4 = False  # True only if len(Ap4) == 1
+
+
+def update_lens():
+    global Len_Ap1, Len_Ap2, Len_Ap3, Len_Ap4
+    global Final_Ap1, Final_Ap2, Final_Ap3, Final_Ap4
+    Len_Ap1 = len(Ap1)
+    Len_Ap2 = len(Ap2)
+    Len_Ap3 = len(Ap3)
+    Len_Ap4 = len(Ap4)
+
+    Final_Ap1 = True if Len_Ap1 == 1 else False
+    Final_Ap2 = True if Len_Ap2 == 1 else False
+    Final_Ap3 = True if Len_Ap3 == 1 else False
+    Final_Ap4 = True if Len_Ap4 == 1 else False
+
+
+def any_update_happen():
+    local_len_ap1 = len(Ap1)
+    local_len_ap2 = len(Ap2)
+    local_len_ap3 = len(Ap3)
+    local_len_ap4 = len(Ap4)
+
+    if any([
+        local_len_ap1 < Len_Ap1,
+        local_len_ap2 < Len_Ap2,
+        local_len_ap3 < Len_Ap3,
+        local_len_ap4 < Len_Ap4
+    ]):
+        return True
 
 
 def generate_new_guess():
@@ -38,11 +81,97 @@ def generate_new_guess():
         S = str(p1) + str(p2) + str(p3) + str(p4)
 
 
+def try_reduce_choices_if_final_true():
+    update_lens()
+    if Final_Ap1:
+        if Ap1[0] in Ap2: Ap2.remove(Ap1[0])
+        if Ap1[0] in Ap3: Ap3.remove(Ap1[0])
+        if Ap1[0] in Ap4: Ap4.remove(Ap1[0])
+    if Final_Ap2:
+        if Ap2[0] in Ap1: Ap1.remove(Ap2[0])
+        if Ap2[0] in Ap3: Ap3.remove(Ap2[0])
+        if Ap2[0] in Ap4: Ap4.remove(Ap2[0])
+    if Final_Ap3:
+        if Ap3[0] in Ap1: Ap1.remove(Ap3[0])
+        if Ap3[0] in Ap2: Ap2.remove(Ap3[0])
+        if Ap3[0] in Ap4: Ap4.remove(Ap3[0])
+    if Final_Ap4:
+        if Ap4[0] in Ap1: Ap1.remove(Ap4[0])
+        if Ap4[0] in Ap2: Ap2.remove(Ap4[0])
+        if Ap4[0] in Ap3: Ap3.remove(Ap4[0])
+    return any_update_happen()
+
+
+def try_reduce_choices_if_cows_0():
+    update_lens()
+    for b1 in range(0, len(Bulls_1_Cows_0)):
+        fp1, fp2, fp3, fp4 = Bulls_1_Cows_0[b1]
+        if Final_Ap1 and Ap1[0] == fp1:
+            if fp2 in Ap2: Ap2.remove(fp2)
+            if fp3 in Ap3: Ap3.remove(fp3)
+            if fp4 in Ap4: Ap4.remove(fp4)
+        if Final_Ap2 and Ap2[0] == fp2:
+            if fp1 in Ap1: Ap1.remove(fp1)
+            if fp3 in Ap3: Ap3.remove(fp3)
+            if fp4 in Ap4: Ap4.remove(fp4)
+        if Final_Ap3 and Ap3[0] == fp3:
+            if fp1 in Ap1: Ap1.remove(fp1)
+            if fp2 in Ap2: Ap2.remove(fp2)
+            if fp4 in Ap4: Ap4.remove(fp4)
+        if Final_Ap4 and Ap4[0] == fp4:
+            if fp1 in Ap1: Ap1.remove(fp1)
+            if fp2 in Ap2: Ap2.remove(fp2)
+            if fp3 in Ap3: Ap3.remove(fp3)
+
+    for b2 in range(0, len(Bulls_2_Cows_0)):
+        fp1, fp2, fp3, fp4 = Bulls_2_Cows_0[b2]
+        if all([Final_Ap1, Final_Ap2, Ap1[0] == fp1, Ap2[0] == fp2]):
+            if fp3 in Ap3: Ap3.remove(fp3)
+            if fp4 in Ap4: Ap4.remove(fp4)
+        if all([Final_Ap1, Final_Ap3, Ap1[0] == fp1, Ap3[0] == fp3]):
+            if fp2 in Ap2: Ap2.remove(fp2)
+            if fp4 in Ap4: Ap4.remove(fp4)
+        if all([Final_Ap1, Final_Ap4, Ap1[0] == fp1, Ap4[0] == fp4]):
+            if fp2 in Ap2: Ap2.remove(fp2)
+            if fp3 in Ap3: Ap3.remove(fp3)
+        if all([Final_Ap2, Final_Ap3, Ap2[0] == fp2, Ap3[0] == fp3]):
+            if fp1 in Ap1: Ap1.remove(fp1)
+            if fp4 in Ap4: Ap4.remove(fp4)
+        if all([Final_Ap2, Final_Ap4, Ap2[0] == fp2, Ap4[0] == fp4]):
+            if fp1 in Ap1: Ap1.remove(fp1)
+            if fp3 in Ap3: Ap3.remove(fp3)
+        if all([Final_Ap3, Final_Ap4, Ap3[0] == fp3, Ap4[0] == fp4]):
+            if fp1 in Ap1: Ap1.remove(fp1)
+            if fp2 in Ap2: Ap2.remove(fp2)
+
+    for b3 in range(0, len(Bulls_3_Cows_0)):
+        fp1, fp2, fp3, fp4 = Bulls_3_Cows_0[b3]
+        if all([Final_Ap1, Final_Ap2, Final_Ap3,
+                Ap1[0] == fp1, Ap2[0] == fp2, Ap3[0] == fp3
+                ]):
+            if fp4 in Ap4: Ap4.remove(fp4)
+        if all([Final_Ap1, Final_Ap2, Final_Ap4,
+                Ap1[0] == fp1, Ap2[0] == fp2, Ap4[0] == fp4
+                ]):
+            if fp3 in Ap3: Ap3.remove(fp3)
+        if all([Final_Ap1, Final_Ap3, Final_Ap4,
+                Ap1[0] == fp1, Ap3[0] == fp3, Ap4[0] == fp4
+                ]):
+            if fp2 in Ap2: Ap2.remove(fp2)
+        if all([Final_Ap2, Final_Ap3, Final_Ap4,
+                Ap2[0] == fp2, Ap3[0] == fp3, Ap4[0] == fp4
+                ]):
+            if fp1 in Ap1: Ap1.remove(fp1)
+
+    return any_update_happen()
+
+
 def try_reduce_choices():
     """
     find_patterns function try to reduce every position choices
     based on information about bulls and cows it gets from the user
     """
+    update_lens()
     global Ap1, Ap2, Ap3, Ap4
 
     for i1 in range(0, len(Bulls_1)):  # Bulls_1
@@ -165,9 +294,11 @@ def try_reduce_choices():
             Ap1 = [fp1]
             Ap2 = [fp2]
             Ap3 = [fp3]  # 1&2&3
+    return any_update_happen()
 
 
 while True:
+
     generate_new_guess()
     bulls, cows = 0, 0
     print(f'Guess -> {loop_flag} is {S}')
@@ -193,7 +324,7 @@ while True:
                 print(
                     "Please enter integer between 0 and 4 and bulls "
                     "and cows together between 0 and 4"
-                      )
+                )
                 continue
         except:
             print("please enter integer")
@@ -204,7 +335,7 @@ while True:
     if bulls == 4:
         print("End Of Game :GG")
         break
-# -----------------------------------------------------------------
+    # -----------------------------------------------------------------
     if bulls == 0:
         Ap1.remove(p1)
         Ap2.remove(p2)
@@ -222,14 +353,25 @@ while True:
         Ap2 = [n for n in Ap2 if n in Current_Guess]
         Ap3 = [n for n in Ap3 if n in Current_Guess]
         Ap4 = [n for n in Ap4 if n in Current_Guess]
-# -----------------------------------------------------------------
+    # -----------------------------------------------------------------
     if bulls == 1:
         Bulls_1.append(Current_Guess)
     elif bulls == 2:
         Bulls_2.append(Current_Guess)
     elif bulls == 3:
         Bulls_3.append(Current_Guess)
-# -----------------------------------------------------------------
-    try_reduce_choices()
+
+    if bulls == 1 and cows == 0:
+        Bulls_1_Cows_0.append(Current_Guess)
+    elif bulls == 2 and cows == 0:
+        Bulls_2_Cows_0.append(Current_Guess)
+    elif bulls == 3 and cows == 0:
+        Bulls_3_Cows_0.append(Current_Guess)
+    # -----------------------------------------------------------------
+    while any([try_reduce_choices_if_final_true(),
+               try_reduce_choices_if_cows_0(),
+               try_reduce_choices()
+               ]):
+        pass
     loop_flag += 1
 # End of while loop
